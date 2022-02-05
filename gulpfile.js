acb50acc879fec68e7f4019ac1f76d2a6d299b66
toono <toono@markernet.co.jp>
2022-02-06 00:33:45 +0900

    px ➝ rem変換

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ const path = process.env.NODE_ENV == "production" ? "out" : "out_dev";
 const htmlReplace = () => {
   return gulp
     .src([`./${path}/**/*.html`])
-    .pipe(replace(/(?<=\/_next\/static\/css\/).*?(?=\.css)/g, "main"))
-    .pipe(replace(/_next\/static\/css\//g, "assets/css/"))
+    .pipe(replace(/(?<=\/_next\/static\/css\/).*?(?=\.css)/g, "main")) // next export時のcssのファイル名を置換
+    .pipe(replace(/_next\/static\/css\//g, "assets/css/")) // next export時のcssのパス名を置換
     .pipe(gulp.dest(`./${path}/`));
 };
 
@@ -20,3 +20,12 @@ const cssRename = () => {
 };
 
 gulp.task("default", gulp.series(gulp.parallel(htmlReplace, cssRename)));
+
+const remConversion = () => {
+  return gulp
+    .src([`./src/**/*.scss`])
+    .pipe(replace(/([1-9]\d*|0)(\.\d+)?px/g, "rem($1$2)")) // 整数 or 少数 + px ➝ rem()に変換
+    .pipe(gulp.dest(`./src/`));
+};
+
+gulp.task("remConversion", gulp.parallel(remConversion));
